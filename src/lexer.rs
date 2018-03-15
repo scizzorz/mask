f66df37fbc3efaa484f09cb8231d36b08fd86600
John Weachock <jweachock@gmail.com>
2018-03-15 16:51:58 -0400

    lexer: Add Token enum

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,50 @@
+#[derive(Debug,Clone,PartialEq)]
+pub enum Token {
+  // Structure
+  EOF,
+  Enter,
+  Exit,
+
+  // Literals
+  Null,
+  Bool(bool),
+  Float(f64),
+  Int(i64),
+  StrLit(String),
+  Name(String),
+
+  // Keywords
+  Break,
+  Continue,
+  Func,
+  If,
+  Return,
+
+  // Symbols
+  Arr, // ->
+  Col, // :
+  Com, // ,
+  Dot, // .
+  Eql, // =
+
+  // Braces
+  Cul, // {
+  Cur, // }
+  Pal, // (
+  Par, // )
+  Sql, // [
+  Sqr, // ]
+
+  // Operators
+  Add, // +
+  Div, // /
+  Mul, // *
+  Sub, // -
+}
+
+use self::Token::*;
+
+
 pub fn lex(path: &str) {
   println!("HELLO I AM LEXING {}", path);
 }
