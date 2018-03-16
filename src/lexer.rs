1f92b8ff3ec6565d5a5d313a621948959be92307
John Weachock <jweachock@gmail.com>
2018-03-16 09:46:18 -0400

    lexer: Add some dummy lexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -16,16 +16,19 @@ pub enum Token {
   // Keywords
   Break,
   Continue,
+  Else,
+  For,
   Func,
   If,
   Return,
+  While,
 
   // Symbols
   Arr, // ->
   Col, // :
   Com, // ,
   Dot, // .
-  Eql, // =
+  Ass, // =
 
   // Braces
   Cul, // {
@@ -40,11 +43,58 @@ pub enum Token {
   Div, // /
   Mul, // *
   Sub, // -
+  Dol, // $
+  Pct, // %
+  Car, // ^
+  And, // &
+  Or,  // |
+  Not, // !
+  Neg, // ~
+
+  // Comparisons
+  Gt,  // >
+  Ge,  // >=
+  Lt,  // <
+  Le,  // <=
+  Ne,  // !=
+  Eql, // ==
 }
 
 use self::Token::*;
 
 
-pub fn lex(path: &str) {
-  println!("HELLO I AM LEXING {}", path);
+pub fn lex(input: &str) {
+  let mut tokens: Vec<Token> = Vec::new();
+  let mut it = input.chars().peekable();
+
+  while let Some(&c) = it.peek() {
+    match c {
+      '-' => {
+        it.next();
+        match it.peek() {
+          Some(&'>') => {
+            tokens.push(Arr);
+            it.next();
+          }
+          _ => {
+            tokens.push(Sub);
+          }
+        }
+      }
+      '+' => {tokens.push(Add); it.next();}
+      '/' => {tokens.push(Div); it.next();}
+      '*' => {tokens.push(Mul); it.next();}
+      '(' => {tokens.push(Pal); it.next();}
+      ')' => {tokens.push(Par); it.next();}
+      '[' => {tokens.push(Sql); it.next();}
+      ']' => {tokens.push(Sqr); it.next();}
+      '{' => {tokens.push(Cul); it.next();}
+      '}' => {tokens.push(Cur); it.next();}
+      '<' => {tokens.push(Lt); it.next();}
+      '>' => {tokens.push(Gt); it.next();}
+      _ => {it.next();}
+    }
+  }
+
+  println!("{:?}", tokens);
 }
