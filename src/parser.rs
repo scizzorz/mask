44b6d1bba980ada36344544e687a788870d70079
John Weachock <jweachock@gmail.com>
2018-03-20 14:38:25 -0400

    parser: Switch to Result instead of my bullshit

--- src/parser.rs
+++ src/parser.rs
@@ -2,8 +2,10 @@ use codemap::Spanned;
 use lexer::Token;
 use std::iter::Peekable;
 use std::slice::Iter;
+use self::ParseErrorKind::*;
 
 type ParseIter<'a> = Peekable<Iter<'a, Spanned<Token>>>;
+type Parse = Result<Node, ParseErrorKind>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Node {
@@ -22,42 +24,13 @@ pub enum Node {
   Int(i64),
   Str(String),
   Name(String),
-
-  None,
 }
+
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ParseErrorKind {
   UnexpectedToken(Token),
   UnexpectedEOF,
-}
-
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Parse {
-  Ok(Node),
-  Err(ParseErrorKind),
-  None
-}
-
-impl Parse {
-  fn expect(self, msg: &str) -> Node {
-    match self {
-      Parse::Ok(n) => n,
-      Parse::Err(x) => panic!("{:?} from {:?}", msg, x),
-      Parse::None => panic!("{:?}", msg),
-    }
-  }
-
-  fn map<F>(self, op: F) -> Parse
-      where F: FnOnce(Node) -> Node {
-    if let Parse::Ok(n) = self {
-      Parse::Ok(op(n))
-    }
-    else {
-      self
-    }
-  }
 }
 
 
@@ -87,50 +60,54 @@ fn use_token(it: &mut ParseIter, kind: Token) -> bool {
 
 
 // Panic if the next token in `it` is *not* `kind`
-fn require_token(it: &mut ParseIter, kind: Token) {
+fn require_token(it: &mut ParseIter, kind: Token) -> Result<(), ParseErrorKind> {
   if let Some(&c) = it.peek() {
-    if c.node != kind {
-      panic!("Expected {:?} but found {:?}", kind, c.node);
-    }
     it.next();
-  } else {
-    panic!("Expected {:?} but found end of stream", kind);
+
+    if c.node == kind {
+      return Ok(());
+    }
+
+    return Err(UnexpectedToken(c.node.clone()));
   }
+
+  return Err(UnexpectedEOF);
 }
 
 
 fn parse_expr(it: &mut ParseIter) -> Parse {
   if let Some(&c) = it.peek() {
     return match c.node {
-      Token::Null => {it.next(); Parse::Ok(Node::Null)},
-      Token::Bool(x) => {it.next(); Parse::Ok(Node::Bool(x))},
-      Token::Float(x) => {it.next(); Parse::Ok(Node::Float(x))},
-      Token::Int(x) => {it.next(); Parse::Ok(Node::Int(x))},
-      Token::Str(ref x) => {it.next(); Parse::Ok(Node::Str(x.clone()))},
-      Token::Name(ref x) => {it.next(); Parse::Ok(Node::Name(x.clone()))},
-      _ => Parse::None,
+      Token::Null => {it.next(); Ok(Node::Null)},
+      Token::Bool(x) => {it.next(); Ok(Node::Bool(x))},
+      Token::Float(x) => {it.next(); Ok(Node::Float(x))},
+      Token::Int(x) => {it.next(); Ok(Node::Int(x))},
+      Token::Str(ref x) => {it.next(); Ok(Node::Str(x.clone()))},
+      Token::Name(ref x) => {it.next(); Ok(Node::Name(x.clone()))},
+      _ => Err(UnexpectedToken(c.node.clone())),
     }
   }
-  Parse::None
+
+  Err(UnexpectedEOF)
 }
 
 
 fn parse_stmt(it: &mut ParseIter) -> Parse {
   if let Some(&c) = it.peek() {
-    match c.node {
+    return match c.node {
       Token::Break => {
         it.next();
-        Parse::Ok(Node::Break)
+        Ok(Node::Break)
       }
 
       Token::Continue => {
         it.next();
-        Parse::Ok(Node::Continue)
+        Ok(Node::Continue)
       }
 
       Token::Pass => {
         it.next();
-        Parse::Ok(Node::Pass)
+        Ok(Node::Pass)
       }
 
       Token::If => {
@@ -143,25 +120,18 @@ fn parse_stmt(it: &mut ParseIter) -> Parse {
       }
     }
   }
-  else {
-    Parse::None
-  }
+
+  Err(UnexpectedEOF)
 }
 
 pub fn parse(tokens: Vec<Spanned<Token>>) -> Parse {
   let mut it: ParseIter = tokens.iter().peekable();
   let mut nodes: Vec<Node> = vec![];
-  loop {
-    match parse_stmt(&mut it) {
-      Parse::Ok(node) => nodes.push(node),
-      Parse::Err(err) => {return Parse::Err(err)},
-      Parse::None => {break},
-    }
 
-    require_token(&mut it, Token::Newline);
-    if peek_token(&mut it, Token::EOF) {
-      break;
-    }
+  while !peek_token(&mut it, Token::EOF) {
+    nodes.push(parse_stmt(&mut it)?);
+    require_token(&mut it, Token::Newline)?;
   }
-  Parse::Ok(Node::Block(nodes))
+
+  Ok(Node::Block(nodes))
 }
