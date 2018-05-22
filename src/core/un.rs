3c8769f82cb4f2a87e2414f9b2b0ca2c20523898
John Weachock <jweachock@gmail.com>
2018-05-22 10:34:23 -0400

    core.un: Add cat, sub, and not operators

--- src/core/un.rs
+++ src/core/un.rs
@@ -23,6 +23,17 @@ pub fn mul(engine: &mut Engine) -> Execute {
 }
 
 pub fn sub(engine: &mut Engine) -> Execute {
+  let val = engine.pop()?;
+
+  use data::Data::*;
+  let ret = match val.val {
+    Int(x) => Int(-x),
+    Float(x) => Float(float::from(-x.into_inner())),
+    _ => return Err(ExecuteErrorKind::BadOperand),
+  };
+
+  engine.data_stack.push(ret.into_item());
+
   Ok(())
 }
 
@@ -31,9 +42,15 @@ pub fn neg(engine: &mut Engine) -> Execute {
 }
 
 pub fn not(engine: &mut Engine) -> Execute {
+  let val = engine.pop()?;
+  let ret = Const::Bool(!val.truth());
+  engine.data_stack.push(ret.into_item());
   Ok(())
 }
 
 pub fn cat(engine: &mut Engine) -> Execute {
+  let val = engine.pop()?;
+  let ret = Const::Str(val.to_string());
+  engine.data_stack.push(ret.into_item());
   Ok(())
 }
